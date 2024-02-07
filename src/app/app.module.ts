ae5f6c8420f3fc83e6424c3baa3fa324ffbfaffb
Дементьева Мария <mdementeva@diasoft.ru>
2024-02-07 17:38:17 +0300

    create project

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    LogoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
---
+++ src/app/types/course.ts
@@ -0,0 +1,8 @@
+export interface Course {
+    id: number,
+    title: string,
+    creationDate: string,
+    //минуты
+    duration: number,
+    description: string,
+}
